0261c9e68144fecb161a32271e286862d7c4d594
Miller <miller@daangn.com>
2022-08-07 20:31:05 +0900

    간단한 lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,56 @@
+package lexer
+
+import "interpreter-go/token"
+
+type Lexer struct {
+	input        string
+	position     int  // 현재 위치
+	readPosition int  // 현재 읽는 위치 (현재 다음 문자)
+	ch           byte // 현재 조사 중인 문자
+}
+
+func NewLexer(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok = newToken(token.EOF, l.ch)
+	}
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.Type, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
--- token/token.go
+++ token/token.go
@@ -1,5 +1,12 @@
 package token
 
+type Type string
+
+type Token struct {
+	Type    Type
+	Literal string
+}
+
 const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
