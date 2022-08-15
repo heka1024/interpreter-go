5f351da932e883ab8ea33ea035adbcd590f63d68
Miller <miller@daangn.com>
2022-08-15 23:59:27 +0900

    Add basic test

---
+++ parser/parser_test.go
@@ -0,0 +1,18 @@
+package parser
+
+import (
+	"interpreter-go/lexer"
+	"testing"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+let x = 5;
+let y = 10;
+let foobar = 838383;
+`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+}
