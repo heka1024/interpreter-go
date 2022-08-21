28f02a7846a19ef2da117bbc1cc1e8b03d046c95
Miller <miller@daangn.com>
2022-08-21 12:58:32 +0900

    Add `return` statement ast

---
+++ ast/return.go
@@ -0,0 +1,14 @@
+package ast
+
+import "interpreter-go/token"
+
+type ReturnStatement struct {
+	Token       token.Token // return token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+
+func (rs *ReturnStatement) tokenLiteral() string {
+	return rs.Token.Literal
+}
