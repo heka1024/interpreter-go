45d4205efe0419440485e7796e6df0133c7ff3ab
Miller <miller@daangn.com>
2022-08-21 13:15:56 +0900

    Add `ExpressionStatement`

---
+++ ast/expression.go
@@ -0,0 +1,14 @@
+package ast
+
+import "interpreter-go/token"
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+func (es *ExpressionStatement) statementNode() {}
