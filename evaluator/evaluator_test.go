6130f9dcbb166c0270a0aa6048352d74f0ab4be3
Miller <miller@daangn.com>
2022-10-02 02:15:00 +0900

    `evalMinuxPrefixOperatorExpression`

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -90,3 +90,18 @@ func TestBangOperator(t *testing.T) {
 		testBooleanObject(t, evaluated, tt.expected)
 	}
 }
+
+func TestMinusOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"-5", -5},
+		{"-10", -10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
