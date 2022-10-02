3036e5dff130b4eaf2570ce2e8c53fa7699b6a94
Miller <miller@daangn.com>
2022-10-02 17:03:01 +0900

    Introduce `Environment`

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -47,6 +47,12 @@ func Eval(node ast.Node) object.Object {
 			return val
 		}
 		return &object.ReturnValue{Value: val}
+	case *ast.LetStatement:
+		val := Eval(node.Value)
+		if object.IsError(val) {
+			return val
+		}
+
 	}
 
 	return nil
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -248,3 +248,42 @@ if (10 > 1) {
 		}
 	}
 }
+
+func TestLetStatement(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let a = 5; a;", 5},
+		{"let a = 5 * 5; a;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
+	}
+}
+
+func TestLetStatementError(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"foobar", "identifier not found: foobar"},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		errObj, ok := evaluated.(*object.Error)
+
+		if !ok {
+			t.Errorf("no error object returned. got=%T (%+v)", evaluated, evaluated)
+			continue
+		}
+
+		if errObj.Message != tt.expected {
+			t.Errorf("wrong error message. expected=%q, got=%q", tt.expected, errObj.Message)
+		}
+	}
+}
